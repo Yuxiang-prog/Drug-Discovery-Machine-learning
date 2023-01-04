c00e943758bc71db235454ba9e78c043627a0467
Yuxiang-prog <85142336+Yuxiang-prog@users.noreply.github.com>
2023-01-04 16:20:41 -0500

    Update ExploratoryDataAnalysis.py

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -65,17 +65,19 @@ df_no_smiles = df.drop(columns='canonical_smiles')
 smiles = []
 for i in df.canonical_smiles.tolist():
   cpd = str(i).split('.')
-  cpd_longest = max(cpd, key=len)
-  smiles.append(cpd_longest)
+  longest = max(cpd, key=len)
+  print(longest)
+  smiles.append(longest)
 
 smiles = pd.Series(smiles, name='canonical_smiles')
 df_clean_smiles = pd.concat([df_no_smiles,smiles], axis=1)
+df_clean_smiles
 
 # Lipinski's Rule
 # Evaluates the drug-likeness of a specific molecule
 
 df_lipinski = lipinski(df_clean_smiles.canonical_smiles)
-println(smiles)
+print(df_lipinski)
 
 df_combined = pd.concat([df,df_lipinski], axis=1)
 # combine the 2 DataFrames
@@ -84,7 +86,7 @@ df_combined.standard_value.describe()
 
 df_norm = norm_value(df_combined)
 df_final = pIC50(df_norm)
-println(smiles)
+print(smiles)
 print(df_final.pIC50.describe())
 
 df_final.to_csv('acetylcholinesterase_04_bioactivity_data_3class_pIC50.csv')
@@ -96,34 +98,33 @@ df_2class.to_csv('acetylcholinesterase_05_bioactivity_data_2class_pIC50.csv')
 # Performing chemical space analysis
 import seaborn as sns
 sns.set(style='ticks')
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as mat
 
-plt.figure(figsize=(5.5, 5.5))
+mat.figure(figsize=(5.5, 5.5))
 
 sns.countplot(x='class', data=df_2class, edgecolor='black')
 
-plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
-plt.ylabel('Frequency', fontsize=14, fontweight='bold')
-println(smiles)
-plt.savefig('plot_bioactivity_class.pdf')
+mat.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+mat.ylabel('Frequency', fontsize=14, fontweight='bold')
+mat.savefig('plot_bioactivity_class.pdf')
 # just offers a visual representation of which inhibitors are labeled as "active" and "inactive"
 
-plt.figure(figsize=(5.5, 5.5))
+mat.figure(figsize=(5.5, 5.5))
 sns.scatterplot(x='MW', y='LogP', data=df_2class, hue='class', size='pIC50', edgecolor='black', alpha=0.7)
 
-plt.xlabel('MW', fontsize=14, fontweight='bold')
-plt.ylabel('LogP', fontsize=14, fontweight='bold')
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0)
-plt.savefig('plot_MW_vs_LogP.pdf')
+mat.xlabel('MW', fontsize=14, fontweight='bold')
+mat.ylabel('LogP', fontsize=14, fontweight='bold')
+mat.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0)
+mat.savefig('plot_MW_vs_LogP.pdf')
 # offers a scatterplot representation to begin training the model
 # is there a relationship between pIC50 value, logP and if the drug inhibitor is active/inactive?
 
-plt.figure(figsize=(5.5, 5.5))
+mat.figure(figsize=(5.5, 5.5))
 sns.boxplot(x = 'class', y = 'pIC50', data = df_2class)
-plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
-plt.ylabel('pIC50 value', fontsize=14, fontweight='bold')
-plt.savefig('plot_ic50.pdf')
-println(smiles)
+mat.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+mat.ylabel('pIC50 value', fontsize=14, fontweight='bold')
+mat.savefig('plot_ic50.pdf')
+print(smiles)
 
 def mannwhitney(descriptor, verbose=False):
     # https://machinelearningmastery.com/nonparametric-statistical-significance-tests-in-python/
@@ -175,37 +176,37 @@ mannwhitney('pIC50')
 # since the p-value is extremely low, we can reject the null hypothesis
 
 # do the same process for all the other gathered values
-plt.figure(figsize=(5.5, 5.5))
+mat.figure(figsize=(5.5, 5.5))
 sns.boxplot(x = 'class', y = 'MW', data = df_2class)
-println(smiles)
-plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
-plt.ylabel('MW', fontsize=14, fontweight='bold')
-plt.savefig('plot_MW.pdf')
+print(smiles)
+mat.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+mat.ylabel('MW', fontsize=14, fontweight='bold')
+mat.savefig('plot_MW.pdf')
 mannwhitney('MW')
 
-plt.figure(figsize=(5.5, 5.5))
+mat.figure(figsize=(5.5, 5.5))
 sns.boxplot(x = 'class', y = 'LogP', data = df_2class)
-println(smiles)
-plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
-plt.ylabel('LogP', fontsize=14, fontweight='bold')
-plt.savefig('plot_LogP.pdf')
+print(smiles)
+mat.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+mat.ylabel('LogP', fontsize=14, fontweight='bold')
+mat.savefig('plot_LogP.pdf')
 mannwhitney('LogP')
 
-plt.figure(figsize=(5.5, 5.5))
+mat.figure(figsize=(5.5, 5.5))
 sns.boxplot(x = 'class', y = 'NumHDonors', data = df_2class)
-plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
-plt.ylabel('NumHDonors', fontsize=14, fontweight='bold')
-plt.savefig('plot_NumHDonors.pdf')
-println(smiles)
+mat.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+mat.ylabel('NumHDonors', fontsize=14, fontweight='bold')
+mat.savefig('plot_NumHDonors.pdf')
+print(smiles)
 mannwhitney('NumHDonors')
 
 
-plt.figure(figsize=(5.5, 5.5))
+mat.figure(figsize=(5.5, 5.5))
 sns.boxplot(x = 'class', y = 'NumHAcceptors', data = df_2class)
-plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
-plt.ylabel('NumHAcceptors', fontsize=14, fontweight='bold')
-println(smiles)
-plt.savefig('plot_NumHAcceptors.pdf')
+mat.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+mat.ylabel('NumHAcceptors', fontsize=14, fontweight='bold')
+print(smiles)
+mat.savefig('plot_NumHAcceptors.pdf')
 print(mannwhitney('NumHAcceptors'))
 
 # all of the above distributions displayed "significant statistical difference"
